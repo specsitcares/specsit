d5e9ad7e65c83f30749423be3ffa0f7ee05893c7
vamshi <vamshikrishna8330@gmail.com>
2026-05-04 00:14:52 +0530

    fix: use CompressedStaticFilesStorage to preserve Vite asset filenames

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -121,7 +121,7 @@ STATICFILES_DIRS = [
     BASE_DIR / 'staticfiles',
     BASE_DIR / 'staticfiles_dist',
 ]
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
